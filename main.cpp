b6c5f3e434ff88c9e4e1945a121e9f45b0a9d54c
pavelleckaya <92338600+pavelleckaya@users.noreply.github.com>
2025-04-14 23:21:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     for(int i = 0; i<=51; ++i){
         std::cout << i << " ";
     }
-    std::cout << "Da zdravstvuet peterburg i eto gorod nash" << std::endl;
+    std::cout << "Da zdravstvuet Sankt-Peterburg i eto gorod nash" << std::endl;
     std::cout << env << std::endl;
 
     return 0;
